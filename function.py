e7a26473cc45bc670b25a9c18f5ddeb749ad43d3
Muhammad Huda <heihudaa@gmail.com>
2018-12-17 17:28:47 +0700

    visualize cq 1 & cq 2

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.options.mode.chained_assignment = None
 
@@ -7,3 +8,23 @@ def getAbb(obj):
     for i, v in enumerate(obj):
         obj[i] = obj[i][obj[i].find("(") + 1:obj[i].find(")")]
     return obj
+
+# Get Total Access each Year
+def getAccessYr(data, limit, column='LastAccess'):
+    access = pd.DatetimeIndex(data[column]).year.value_counts().head(limit).sort_value()
+    return access
+
+# Set text to each plot
+def setPlotText(data, x, y, val='', halign='right', valign='bottom', color='black'):
+    for i, v in enumerate(data):
+        plt.text(i+(x), v+(y), str(v) + val, horizontalalignment=halign, verticalalignment=valign, color=color, fontweight='bold')
+
+# Show plot
+def showPlot(title, xlabel='', ylabel=''):
+    # Set plot title
+    plt.title(title)
+    # Set plot label
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    # Show plot
+    plt.show()
--- main.py
+++ main.py
@@ -8,17 +8,23 @@ import matplotlib.pyplot as plt
 # JUMLAH AKSES SHARE ITS 5 TAHUN TERAKHIR
 # ----------------------------------------------------------------------------------------------
 """
-jmlAkses = pd.DataFrame({
-    'aksesDosen' : pd.DatetimeIndex(db.dosen['LastAccess']).year.value_counts().head(5),
-    'aksesMahasiswa' : pd.DatetimeIndex(db.mahasiswa['LastAccess']).year.value_counts().head(5),
+# --- Akses Dosen & Mahasiswa
+akses = pd.DataFrame({
+    'AksesDosen' : pd.DatetimeIndex(db.dosen['LastAccess']).year.value_counts().head(5),
+    'AksesMahasiswa' : pd.DatetimeIndex(db.mahasiswa['LastAccess']).year.value_counts().head(5),
 })
-jmlAkses['total'] = jmlAkses.aksesDosen + jmlAkses.aksesMahasiswa
-jmlAkses['total'].plot.bar(rot=0)
-plt.title('Jumlah akses Share ITS 5 tahun terakhir')
-plt.ylabel('Jumlah Akses')
-plt.xlabel('Tahun')
-for i, v in enumerate(jmlAkses['total']):
-    plt.text(i, v - 500, v, horizontalalignment='center', color='white', fontweight='bold')
+akses.plot.bar(rot=0)
+plt.yticks(np.arange(0, 16000, 2000))
+func.setPlotText(akses.AksesDosen, x=-0.05, y=300, color='#1F77B4')
+func.setPlotText(akses.AksesMahasiswa, x=0.1, y=200, halign='center', color='#FF7F0E')
+func.showPlot(title='Akses Share ITS Dosen & Mahasiswa 5 tahun terakhir', xlabel='Tahun', ylabel='Jumlah Akses')
+
+# --- Total Akses
+totalAkses = akses.AksesDosen + akses.AksesMahasiswa
+totalAkses.plot.bar(rot=0)
+func.setPlotText(totalAkses, x=0, y=100, halign='center')
+plt.yticks(np.arange(0, 14000, 2000))
+func.showPlot(title='Jumlah akses Share ITS 5 tahun terakhir', xlabel='Tahun', ylabel='Jumlah Akses')
 plt.show()
 """
 # ----------------------------------------------------------------------------------------------
@@ -27,32 +33,49 @@ plt.show()
 # ----------------------------------------------------------------------------------------------
 # AKSES SHARE ITS MAHASISWA TIAP FAKULTAS PADA TAHUN 2018
 # ----------------------------------------------------------------------------------------------
-"""
-data = db.mahasiswa[['Fakultas', 'LastAccess']].groupby(['Fakultas']).size().reset_index(name='Jumlah Mahasiswa')
-data = data[data['Fakultas'].str.match('Fakultas')]
+# Get Jumlah Mahasiswa
+jmlMhs = db.mahasiswa[['Fakultas', 'LastAccess']].groupby(['Fakultas']).size().reset_index(name='Jumlah Mahasiswa')
+jmlMhs = jmlMhs[jmlMhs['Fakultas'].str.match('Fakultas')]
 
+# Get Jumlah Akses Mahasiswa
 mhs = db.mahasiswa
 mhs['LastAccess'] = pd.DatetimeIndex(mhs['LastAccess']).year
-aks2018 = mhs[mhs['LastAccess'] == 2018].groupby(mhs['Fakultas']).size().reset_index(name='Jumlah Akses')
-aks2018 = aks2018[aks2018['Fakultas'].str.match('Fakultas')]
+jmlAkses = mhs[mhs['LastAccess'] == 2018].groupby(mhs['Fakultas']).size().reset_index(name='Jumlah Akses')
+jmlAkses = jmlAkses[jmlAkses['Fakultas'].str.match('Fakultas')]
 
-new = pd.merge(data, aks2018, how='outer')
+# Merge Jumlah Mahasiswa & Jumlah Akses
+jmlAksesMhs = pd.merge(jmlMhs, jmlAkses, how='outer')
 
-percent = np.round(np.array(new['Jumlah Akses']) / np.array(data['Jumlah Mahasiswa']) * 100, 1)
-new.plot.bar(rot=0)
-plt.xticks(np.arange(5), func.getAbb(new['Fakultas']))
-for i, v in enumerate(new['Jumlah Mahasiswa']):
-    plt.text(i, v+100, str(percent[i])+'%', horizontalalignment='center', color='black', fontweight='bold')
-plt.title('Akses Share ITS tiap fakultas pada tahun 2018')
-plt.xlabel('Fakultas')
-plt.show()
-"""
+# Set Plot
+jmlAksesMhs.plot.bar(rot=0)
+plt.xticks(np.arange(5), func.getAbb(jmlAksesMhs['Fakultas']))
+plt.yticks(np.arange(0, 12000, 2000))
+
+# Set Plot Text
+func.setPlotText(jmlAksesMhs['Jumlah Mahasiswa'], x=0, y=200, color='#1F77B4')
+func.setPlotText(jmlAksesMhs['Jumlah Akses'], x=0.05, y=200, halign='left', color='#FF7F0E')
+
+# Show Plot
+func.showPlot(title='Akses Share ITS Mahasiswa tiap fakultas pada tahun 2018', xlabel='Fakultas')
+
+percent = pd.DataFrame({
+    'Fakultas' : func.getAbb(jmlAksesMhs['Fakultas']),
+    'Akses' : np.round(np.array(jmlAksesMhs['Jumlah Akses']) / np.array(jmlAksesMhs['Jumlah Mahasiswa']) * 100, 1),
+})
+percent.plot.bar(rot=0)
+func.setPlotText(percent['Akses'], x=0, y=1, val='%', halign='center')
+
+# Set ticks
+plt.yticks(np.arange(0, 140, 20))
+plt.xticks(np.arange(5), percent['Fakultas'])
+func.showPlot(title='Persentase akses Share ITS Mahasiswa pada tahun 2018', xlabel='Fakultas', ylabel='Akses (%)')
 # ----------------------------------------------------------------------------------------------
 
 
 # ----------------------------------------------------------------------------------------------
 # RATA-RATA WAKTU AKSES MAHASISWA
 # ----------------------------------------------------------------------------------------------
+"""
 mhs = pd.DatetimeIndex(db.mahasiswa['LastAccess']).hour.value_counts().reset_index().sort_values('index')
 waktuAkses = pd.DataFrame({
     'waktu' : ['00:00 - 05:59', '06:00 - 11:59', '12:00 - 17:59', '18:00 - 23:59'],
@@ -65,4 +88,5 @@ plt.xticks(np.arange(4), waktuAkses['waktu'])
 plt.ylabel('Jumlah Akses')
 plt.xlabel('Waktu Akses')
 plt.show()
+"""
 # ----------------------------------------------------------------------------------------------
